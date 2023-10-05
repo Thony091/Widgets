b7011f25570537b2c3941614c15dce95815a56bb
ThonyF <anthony.g91@icloud.com>
2023-10-05 17:01:22 -0300

    colors

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+  const colorList = <Color>[
+    Colors.blue,
+  ];
+  
+class AppTheme{
+
+  final int selectedColor;
+
+  AppTheme({
+    required this.selectedColor, 
+
+  });
+
+
+}
