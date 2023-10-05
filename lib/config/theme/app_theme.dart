ac350c68ae861a759f7c03dab0996f91d41b6f88
ThonyF <anthony.g91@icloud.com>
2023-10-05 17:07:03 -0300

    more colors

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 
   const colorList = <Color>[
     Colors.blue,
+    Colors.pink,
+    Colors.red,
+    Colors.brown,
+    Colors.orange,
+    Colors.purple,
+    Colors.green,
   ];
   
 class AppTheme{
