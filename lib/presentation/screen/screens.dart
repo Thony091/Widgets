14ad19b557f9a2599e83ca52e9b4ad91aeceb035
ThonyF <anthony.g91@icloud.com>
2023-10-06 15:01:41 -0300

    add screens

--- lib/presentation/screen/screens.dart
+++ lib/presentation/screen/screens.dart
@@ -1,3 +1,10 @@
+
+export 'package:widgets/presentation/screen/animated/animated_screen.dart';
+export 'package:widgets/presentation/screen/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets/presentation/screen/buttons/buttons_screen.dart';
 export 'package:widgets/presentation/screen/cards/cards_screen.dart';
 export 'package:widgets/presentation/screen/home/home_screen.dart';
+export 'package:widgets/presentation/screen/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets/presentation/screen/progress/progress_screen.dart';
+export 'package:widgets/presentation/screen/snackbar/snackbar_screen.dart';
+export 'package:widgets/presentation/screen/ui_controls/ui_controls_screen.dart';
