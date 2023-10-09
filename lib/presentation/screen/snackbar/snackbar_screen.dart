edf139f901aece17f5ef0b4b724f61639a8067c4
ThonyFlores <anthony.g91@icloud.com>
2023-10-09 16:43:28 -0300

    avance wdg´s  'snackbar complete'

--- lib/presentation/screen/snackbar/snackbar_screen.dart
+++ lib/presentation/screen/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
 
@@ -18,12 +19,48 @@ class SnackbarScreen extends StatelessWidget {
 
     ScaffoldMessenger.of(context).showSnackBar(snackBar); //Muestra el widget que se requiere
   }
-
+  void openDialog(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => AlertDialog( // *** Builder = Algo que se va a construir en tiempo de ejecución ***
+        title: const Text('¿Estas Seguro de realizar esta acción?'),
+        content: const Text('Nulla proident anim et fugiat ut excepteur deserunt elit fugiat nulla nisi.'),
+        actions: [
+          TextButton(onPressed: () => context.pop(), child: const Text('Cancelar')),
+          FilledButton(onPressed: () => context.pop(), child: const Text('Aceptar'))
+        ],
+      ),
+    ); 
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('SnackBar y Dialogos'),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center, //para centrar los children
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  children: [
+                  const Text('Mollit laboris enim amet ad nostrud voluptate laborum.')
+                  ]
+                );
+              }, 
+              child: const Text('Licencias Usadas')
+            ),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context), 
+              child: const Text('Mostrar Dialogo')
+            ),
+          ],
+        ),
       ),
 
       floatingActionButton: FloatingActionButton.extended(
